36bae3558a1d87c6500cc1e8d8eac7c11ee4b0ab
Patrick Morpey <patrick@morphp.nl>
2023-12-15 15:35:53 +0100

    Added test

---
+++ tests/Functional/BasicTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\JsonTest\Functional;
+
+use Medas\Json\JsonEncoder;
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testBasicArray(): void
+    {
+        $encoder = service(JsonEncoder::class);
+
+        $data = [
+            'a' => 'b',
+            1 => 2,
+            '€',
+            "non-unicode bytes \xF0\xA4\xAD\xA2\xF0\xA4\xAD\xA2\xF0\xA4\xAD",
+        ];
+
+        self::assertEquals($data, $encoder->decode($encoder->encode($data)));
+        self::assertEquals($data, $encoder->decode($encoder->encode($data, true)));
+    }
+}
